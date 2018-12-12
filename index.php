2554dcfed5e30d1b1deac195596387142647652c
Wendy <45633033+Wendybbc@users.noreply.github.com>
2018-12-12 18:03:19 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,48 @@
-<meta-charset="utf-8"/>
+<html>
+
+	<head>
+	
+		<meta-charset="utf-8"/>
+	
+	</head>
+
+<body>
+	
+	<form method=post>
+	
+	
+	<b> Veuillez saisir le code de sécurité : </b> <br>
+	
+	
 
 <?php
-
-/* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
-* AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
-
+session_start();
 echo "<img src='script-captchas.php' alt='captchas' />";
 
+	
+//	if(isset($_REQUEST['envoyer']))
+//	{
+//		$captchas=$_RESQUEST;
+//		if($captchas=="")
+//		{
+//			echo "Aucune valeur";
+//		}
+//		else
+//		{
+//			if($captchas==captchas)
+//		{
+//			echo "Code valide";
+//		}
+//		}
+//	}
+		
 ?>
+
+	<br>
+	<input type="text" name="text"> 
+	<input type="submit" value="envoyer" name="envoyer">
+	
+</body>
+	
+
+</html>
